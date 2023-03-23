d6a9b0e8d51de29c439887e1e84709fee93e711f
Uzoma Jeremiah Chike <mcjerry73@gmail.com>
2023-03-23 15:27:37 +0100

    Update interact.js

--- src/web3/interact.js
+++ src/web3/interact.js
@@ -6,45 +6,46 @@ import {createAndLoginUser} from '../networking/user'
 const config = require('../config/config')[process.env.NODE_ENV || 'development'];
 
 let web3auth;
-const clientId = "BDzbYt1CVIUyalELrMFxpDJAEU9RAV3CY78Jm-uX8Ly9L7lNtcLqyWsE-ajhuVn_hqJMJ8zgKGhSHq1iVonTgH0";
+const clientId = config.clientId;
  const init = async () => {
           try {   
              web3auth = new Web3Auth({
-        clientId,
-              web3AuthNetwork: "cyan",
-              chainConfig: {
-    chainNamespace: "eip155",
-
-    rpcTarget: "https://rpc.ankr.com/eth_goerli",
-    displayName: "Goerli Testnet",
-    blockExplorer: "https://goerli.etherscan.io",
-    ticker: "ETH",
-    tickerName: "Ethereum",
-  },
+                // type uiConfig
+               uiConfig: {
+               appLogo: config.appLogo,
+               theme: "dark",
+               loginMethodsOrder: ["google", "facebook"],
+               defaultLanguage: "en",
+             },
+               clientId,
+               chainConfig: { // this is ethereum chain config, change if other chain(Solana, Polygon)
+                   chainNamespace: CHAIN_NAMESPACES.EIP155,
+                   chainId: config.chainId,
+                   rpcTarget: config.rpcTarget,
+               }
              });
-               await web3auth.initModal();
+    
+            await web3auth.initModal();
             if (web3auth.provider) {
             };
           } catch (error) {
             console.error(error);
           }
         };
+  init();
 
- init();
    export const connectWallet = async () => {
-     
         if (!web3auth) {
             console.log("web3auth not initialized yet");
             return;
         }
-         const provider = await web3auth.connect();
+        const provider = await web3auth.connect();
         // if provider is not null then user logged in successfully
         if(provider != null){
             console.log('User logged in successfully.');
          }
           const web3 = new Web3(provider);
           const userAccounts = await web3.eth.getAccounts();
-          console.log(userAccounts);
           const user = await web3auth.getUserInfo();
           const address = userAccounts[0];
           user.address=address;
@@ -64,24 +65,3 @@ const clientId = "BDzbYt1CVIUyalELrMFxpDJAEU9RAV3CY78Jm-uX8Ly9L7lNtcLqyWsE-ajhuV
 
       }
   }
-  export const signTransaction = async () => {   
-    if (!web3auth) {
-        console.log("web3auth not initialized yet");
-        return;
-      }
-  
-      const provider = await web3auth.connect();
-      const web3 = new Web3(provider);
-
-      const fromAddress = (await web3.eth.getAccounts())[0];
-      const originalMessage = "By signing below, I acknowledge that I have read and understand the content of this agreement";
-      return new Promise((resolve, reject) => {
-        web3.eth.personal.sign(originalMessage, fromAddress).then(signedMessage => {
-         resolve({status: true, message: "Message signed successfully", signedMessage: signedMessage}) 
-      }).catch(error => {
-        reject({status: false, message: error.message || "Could not sign the message."});
-    });
-    })  
-  }
-
-  
